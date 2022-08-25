4844272679b013fc23c9d295c39fa953c9b6b335
“Danillo” <danillo.abreu@arapiraca.ufal.br>
2022-08-25 11:09:50 -0300

    Using Nodemon

--- 01-fundamentos-do-nodejs/src/index.js
+++ 01-fundamentos-do-nodejs/src/index.js
@@ -1,10 +1,9 @@
-const { application, request } = require('express');
 const express = require('express');
 
 const app = express();
 
 app.get("/", (request, response) =>{
-    return response.json({message: "Hello Word Ignite!"});
+    return response.json({message: "Hello World Ignite - Fundamentos NodeJS!"});
 });
 
 //localhost:3333
