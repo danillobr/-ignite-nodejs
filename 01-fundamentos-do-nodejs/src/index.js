cf6e2d43b49ca62fddd945a441d576534846da51
“Danillo” <danillo.abreu@arapiraca.ufal.br>
2022-08-25 11:32:39 -0300

    Utilizando os métodos HTTP

--- 01-fundamentos-do-nodejs/src/index.js
+++ 01-fundamentos-do-nodejs/src/index.js
@@ -1,10 +1,57 @@
+const { request, response } = require('express');
 const express = require('express');
 
 const app = express();
 
-app.get("/", (request, response) =>{
-    return response.json({message: "Hello World Ignite - Fundamentos NodeJS!"});
+/**
+ * GET - Buscar uma informação dentro do servidor
+ * POST - Inserir uma informação no  servidor
+ * PUT - Alterar uma informação no servidor
+ * PATH - Alterar uma informação especifica no servidor
+ */
+
+app.get("/courses", (request, response) => {
+    return response.json([
+        "Curso 1",
+        "Curso 2",
+        "Curso 3"
+    ]);
 });
+
+app.post("/courses", (request, respnse) => {
+    return response.json([
+        "Curso 1",
+        "Curso 2",
+        "Curso 3",
+        "Curso 4"
+    ]);
+});
+
+app.put("courses/:id", (request, response) => {
+    return response.json([
+        "Curso 6",
+        "Curso 2",
+        "Curso 3",
+        "Curso 4"
+    ]);
+})
+
+app.path("courses/:id", (request, response) => {
+    return response.json([
+        "Curso 6",
+        "Curso 7",
+        "Curso 3",
+        "Curso 4"
+    ]);
+})
+
+app.path("courses/:id", (request, response) => {
+    return response.json([
+        "Curso 6",
+        "Curso 7",
+        "Curso 4"
+    ]);
+})
 
 //localhost:3333
 app.listen(3333);
