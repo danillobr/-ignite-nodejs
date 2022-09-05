b5292ceedb35019c11dbf2f95b7b7375a02d3d79
“Danillo” <danillo.abreu@arapiraca.ufal.br>
2022-09-05 15:23:43 -0300

    Primeiro projeto com Node.js

--- 02-primeiro-projeto-nodejs/src/index.js
+++ 02-primeiro-projeto-nodejs/src/index.js
@@ -1,5 +1,40 @@
 const express = require("express");
+const { v4: uuidv4 } = require("uuid")
 
-const app = experss();
+const app = express()
 
-app.listen(3333)
+app.use(express.json())
+
+const customers = [];
+
+/**
+ * cpf - string
+ * name - string
+ * id - uuid
+ * statement []
+ */
+
+app.post("/account", (request, response) => {
+    const { cpf, name } = request.body;
+
+    const customerAlreadyExists = customers.some(
+        (customer) => customer.cpf === cpf
+    );
+    
+    if(customerAlreadyExists){
+        return response.status(400).json({error: "Customer already exists!"});
+    }
+
+    const id = uuidv4();
+
+    customers.push({
+        cpf,
+        name,
+        id,
+        statement: [],
+    });
+
+    return response.status(201).send();
+});
+
+app.listen(3333);
