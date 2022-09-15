4d6a4056b47fe798184b94db1c0374d4d6144caa
danillobr <danillorodriguesabreu@gmail.com>
2022-09-15 14:55:03 -0300

    Listando as categorias

--- 03-iniciando-api/src/routes/categories.routes.ts
+++ 03-iniciando-api/src/routes/categories.routes.ts
@@ -1,4 +1,4 @@
-import { Router } from "express";
+import { request, response, Router } from "express";
 
 import { CategoriesRepository } from "../repositories/CategortiesRepository";
 
@@ -12,6 +12,12 @@ categoriesRoutes.post("/", (request, response) => {
     categoriesRepository.create({ name, description });
 
     return response.status(201).send();
+});
+
+categoriesRoutes.get("/", (request, response) => {
+    const all = categoriesRepository.list();
+
+    return response.json(all);
 });
 
 export { categoriesRoutes };
