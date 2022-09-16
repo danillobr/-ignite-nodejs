9647687f60427d1a0b869dfe564cb15468eae751
danillobr <danillorodriguesabreu@gmail.com>
2022-09-16 17:57:09 -0300

    Criando os Use Case de Categoria

--- 03-iniciando-api/src/routes/categories.routes.ts
+++ 03-iniciando-api/src/routes/categories.routes.ts
@@ -1,22 +1,14 @@
 import { Router } from "express";
 
 import { CategoriesRepository } from "../Modules/cars/repositories/CategortiesRepository";
-import { CreateCategoryService } from "../Modules/cars/Services/CreateCategoryService";
+import { createCategoryController } from "../Modules/cars/useCases/CreateCategory";
 
 const categoriesRoutes = Router();
 
 const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post("/", (request, response) => {
-    const { name, description } = request.body;
-
-    const createCategoryService = new CreateCategoryService(
-        categoriesRepository
-    );
-
-    createCategoryService.execute({ name, description });
-
-    return response.status(201).send();
+    return createCategoryController.handle(request, response);
 });
 
 categoriesRoutes.get("/", (request, response) => {
