cf87bda1c33ea0fa103ec1854496472bf419754e
danillobr <danillorodriguesabreu@gmail.com>
2022-09-16 15:35:09 -0300

    Criando repositório de especificação

--- 03-iniciando-api/src/Modules/cars/Services/CreateSpecificationService.ts
+++ 03-iniciando-api/src/Modules/cars/Services/CreateSpecificationService.ts
@@ -1,7 +1,25 @@
-class CreateSpecification {
-    execute() {
-        console.log("Todo");
+import { ISpecificationsRepository } from "../repositories/ISpecificationsRepository";
+
+interface IRequest {
+    name: string;
+    description: string;
+}
+class CreateSpecificationService {
+    private specificationsRepository;
+
+    constructor(specificationsRepository: ISpecificationsRepository) {
+        this.specificationsRepository = specificationsRepository;
+    }
+    execute({ name, description }: IRequest): void {
+        const specificationAlreadyExists =
+            this.specificationsRepository.findByName(name);
+
+        if (specificationAlreadyExists) {
+            throw new Error("Specification already exists!");
+        }
+
+        this.specificationsRepository.create({ name, description });
     }
 }
 
-export { CreateSpecification };
+export { CreateSpecificationService };
