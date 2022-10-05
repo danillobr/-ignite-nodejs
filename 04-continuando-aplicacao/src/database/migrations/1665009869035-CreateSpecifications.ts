312169333f0035a3365df376f6c6e52762956091
danillobr <danillorodriguesabreu@gmail.com>
2022-10-05 20:21:00 -0300

    Criando migration de especificação

---
+++ 04-continuando-aplicacao/src/database/migrations/1665009869035-CreateSpecifications.ts
@@ -0,0 +1,35 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class CreateSpecifications1665009869035 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: "specifications",
+        columns: [
+          {
+            name: "id",
+            type: "uuid",
+            isPrimary: true,
+          },
+          {
+            name: "nome",
+            type: "varchar",
+          },
+          {
+            name: "description",
+            type: "varchar",
+          },
+          {
+            name: "created_at",
+            type: "timestamp",
+            default: "now()",
+          },
+        ],
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable("specifications");
+  }
+}
