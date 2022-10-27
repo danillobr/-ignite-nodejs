07b1845390af9468582d7a01ab4fe875b0bc58b1
danillobr <danillorodriguesabreu@gmail.com>
2022-10-27 16:41:59 -0300

    Autenticação nas rotas

--- 04-continuando-aplicacao/src/middlewares/ensureAuthenticated.ts
+++ 04-continuando-aplicacao/src/middlewares/ensureAuthenticated.ts
@@ -1,6 +1,12 @@
 import { Response, Request, NextFunction } from "express";
 import { verify } from "jsonwebtoken";
 
+import { UsersRepository } from "../modules/accounts/repositories/implementations/UsersRepository";
+
+interface IPayload {
+  sub: string;
+}
+
 export async function ensureAuthenticated(
   request: Request,
   response: Response,
@@ -15,8 +21,20 @@ export async function ensureAuthenticated(
   const [, token] = authHeader.split(" ");
 
   try {
-    const decoded = verify(token, "0074f7cdcb8b471873ff63e809352e2d");
-    console.log(decoded);
+    const { sub: user_id } = verify(
+      token,
+      "0074f7cdcb8b471873ff63e809352e2d"
+    ) as IPayload;
+
+    const usersRepository = new UsersRepository();
+
+    const user = usersRepository.findById(user_id);
+
+    if (!user) {
+      throw new Error("User does not exists!");
+    }
+
+    next();
   } catch {
     throw new Error("Invalid token");
   }
