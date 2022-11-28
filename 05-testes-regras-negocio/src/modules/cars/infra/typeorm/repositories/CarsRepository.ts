ed19f27dbadf025d37d1cf4925e563b91f895ea7
danillobr <danillorodriguesabreu@gmail.com>
2022-11-28 18:52:45 -0300

    Continuação da listagem de carros disponíveis

--- 05-testes-regras-negocio/src/modules/cars/infra/typeorm/repositories/CarsRepository.ts
+++ 05-testes-regras-negocio/src/modules/cars/infra/typeorm/repositories/CarsRepository.ts
@@ -39,6 +39,32 @@ class CarsRepository implements ICarsRepository {
   async findByLicensePlate(license_plate: string): Promise<Car> {
     return this.repository.findOne({ license_plate });
   }
+
+  async findByAvailable(
+    brand?: string,
+    category_id?: string,
+    name?: string
+  ): Promise<Car[]> {
+    const crasQuery = await this.repository
+      .createQueryBuilder("c")
+      .where("available = :available", { available: true });
+
+    if (brand) {
+      crasQuery.andWhere("c.brand = :brand", { brand });
+    }
+
+    if (category_id) {
+      crasQuery.andWhere("c.category_id = :category_id", { category_id });
+    }
+
+    if (name) {
+      crasQuery.andWhere("c.name = :name", { name });
+    }
+
+    const cars = await crasQuery.getMany();
+
+    return cars;
+  }
 }
 
 export { CarsRepository };
