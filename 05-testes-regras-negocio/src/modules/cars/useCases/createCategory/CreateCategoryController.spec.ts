b4068b6f373e43bd3d43e721632598c1efbb2db0
danillobr <danillorodriguesabreu@gmail.com>
2022-12-16 17:42:34 -0300

    Continuando teste de integração

--- 05-testes-regras-negocio/src/modules/cars/useCases/createCategory/CreateCategoryController.spec.ts
+++ 05-testes-regras-negocio/src/modules/cars/useCases/createCategory/CreateCategoryController.spec.ts
@@ -33,12 +33,17 @@ describe("Create Category Controller", () => {
       password: "admin",
     });
 
-    console.log(responseToken.body);
+    const { token } = responseToken.body;
 
-    const response = await request(app).post("/categories").send({
-      name: "Category supertest",
-      description: "Category supertest",
-    });
+    const response = await request(app)
+      .post("/categories")
+      .send({
+        name: "Category supertest",
+        description: "Category supertest",
+      })
+      .set({
+        Authorization: `Bearer ${token}`,
+      });
 
     expect(response.status).toBe(201);
   });
