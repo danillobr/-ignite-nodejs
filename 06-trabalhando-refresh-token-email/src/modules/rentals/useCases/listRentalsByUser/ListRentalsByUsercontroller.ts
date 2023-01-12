ad9e14b54dd6d551e890737a89fd49f729862776
danillobr <danillorodriguesabreu@gmail.com>
2023-01-12 17:26:32 -0300

    Listagem de aluguéis do usuário

--- 06-trabalhando-refresh-token-email/src/modules/rentals/useCases/listRentalsByUser/ListRentalsByUsercontroller.ts
+++ 06-trabalhando-refresh-token-email/src/modules/rentals/useCases/listRentalsByUser/ListRentalsByUsercontroller.ts
@@ -1,22 +1,10 @@
-<<<<<<< HEAD
 import { request, Request, Response } from "express";
 import { container } from "tsyringe";
 
-import { Rental } from "@modules/rentals/infra/typeorm/entities/Rental";
-
-import { ListRentalsByUserUseCase } from "./ListRentalsByUserUseCase";
-
-class ListRentalsByUserController {
-  async handle(request: Request, response: Response): Promise<Response> {
-=======
-import { Rental } from "@modules/rentals/infra/typeorm/entities/Rental";
-import { request, Request, Response } from "express";
-import { container } from "tsyringe";
 import { ListRentalsByUserUseCase } from "./ListRentalsByUserUseCase";
 
 class ListRentalsByUserController {
   async handle(rquest: Request, response: Response): Promise<Response> {
->>>>>>> ddbe17f9186ae22a41bd07e75f67972013962ac4
     const { id } = request.user;
     const listRentalsByUserUseCase = container.resolve(
       ListRentalsByUserUseCase
--- 06-trabalhando-refresh-token-email/src/shared/infra/http/routes/rental.routes.ts
+++ 06-trabalhando-refresh-token-email/src/shared/infra/http/routes/rental.routes.ts
@@ -5,7 +5,6 @@ import { DevolutionRentalController } from "@modules/rentals/useCases/devolution
 import { ListRentalsByUserController } from "@modules/rentals/useCases/listRentalsByUser/ListRentalsByUsercontroller";
 
 import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
-import { ListRentalsByUserController } from "@modules/rentals/useCases/listRentalsByUser/ListRentalsByUsercontroller";
 
 const rentalsRoutes = Router();
 
@@ -20,11 +19,7 @@ rentalsRoutes.post(
   devolutionRentalController.handle
 );
 rentalsRoutes.get(
-<<<<<<< HEAD
-  "/user",
-=======
   "/user/",
->>>>>>> ddbe17f9186ae22a41bd07e75f67972013962ac4
   ensureAuthenticated,
   listRentalsByUserController.handle
 );
