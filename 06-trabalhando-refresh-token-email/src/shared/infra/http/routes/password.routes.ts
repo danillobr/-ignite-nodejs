2092ef3f834dc931b91046e4d5511a66b30daedb
danillobr <danillorodriguesabreu@gmail.com>
2023-02-07 15:39:23 -0300

    Testando envio de e-mail

--- 06-trabalhando-refresh-token-email/src/shared/infra/http/routes/password.routes.ts
+++ 06-trabalhando-refresh-token-email/src/shared/infra/http/routes/password.routes.ts
@@ -1,7 +1,7 @@
 import { Router } from "express";
 
 import { ResetPasswordUserController } from "@modules/accounts/useCases/resetPasswordUser/ResetPasswordUserController";
-import { SendForgotPasswordMailController } from "@modules/accounts/useCases/sendForgotPasswordMail/sendForgotPasswordMailControllerts";
+import { SendForgotPasswordMailController } from "@modules/accounts/useCases/sendForgotPasswordMail/SendForgotPasswordMailController";
 
 const passwordRoutes = Router();
 
